7134b34a15fc76d8e48ad3a31af42b5f00f3a97c
MARS <61511540+MARSMPEI@users.noreply.github.com>
2022-09-19 07:54:28 +0300

    Update Control_of_robots_lab1.cpp
    Убрал хлам, добавил функции с роботом

--- Lab_1/Control_of_robots_lab1/Control_of_robots_lab1.cpp
+++ Lab_1/Control_of_robots_lab1/Control_of_robots_lab1.cpp
@@ -8,9 +8,6 @@
 #include <algorithm>
 #include <fstream>
 
-
-
-
 using namespace std;
 
 unsigned int lenght_of_vector(); //узнаем количество строк в входном файле
@@ -18,6 +15,9 @@ void open_file(vector<int>& Vx, vector<int>& Vy, vector<int>& OM); // откры
 void correct_file(const ifstream& file); // проверяем наличие входного файла
 void push_data_to_output(); // помещение данных в выходной файл
 void digit_separator(string str, vector<int>& Vx, vector<int>& Vy, vector<int>& OM, unsigned int counter); // разделение входных данных на три и запись их в соответствующие векторы
+void vector_output(vector<int>& Vx, string name_of_vector); // вывод векторов на консоль
+//void push_data_to_youBot(vector<int>& Vx, vector<int>& Vy, vector<int>& OM); // помещение данных из векторов в youBot
+//void get_data_by_youBot(); // снятие данных с робота
 
 int main()
 {
@@ -29,28 +29,11 @@ int main()
     string output = "output.txt";
     ofstream output_status_data;
     output_status_data.open(output, fstream::out);
+   // push_data_to_youBot(Vx,Vy,OM);
     
-    int i = 0;
-    cout << "Vx: " << endl;
-    while (i < Vx.size())
-    {
-        cout << Vx[i] << endl;
-        i++;
-    }
-    i = 0;
-    cout << "Vy: " << endl;
-    while (i < Vy.size())
-    {
-        cout << Vy[i] << endl;
-        i++;
-    }
-    cout << "OM: " << endl;
-    i = 0;
-    while (i < OM.size())
-    {
-        cout << OM[i] << endl;
-        i++;
-    }
+    //vector_output(Vx, "Vx");
+    //vector_output(Vy, "Vy");
+    //vector_output(OM, "OM");
 }
 unsigned int lenght_of_vector()
 {
@@ -64,10 +47,20 @@ unsigned int lenght_of_vector()
         lenght_of_vector++;
     }
     input_data_of_velosity.seekg(0);
-    cout << "lenght_of_vector = " << lenght_of_vector << endl;
+    //cout << "lenght_of_vector = " << lenght_of_vector << endl;
     input_data_of_velosity.clear();
     input_data_of_velosity.seekg(0L, std::ios_base::beg); // после чтения файла возвращаемся к первой строке, когда писал все в open_file - было необходимо, возможно в функции не требуется, но проверять лень 
     return(lenght_of_vector);
+}
+void vector_output(vector<int>& Vx, string name_of_vector)
+{
+    int i = 0;
+    cout << "Vector: "<< name_of_vector << endl;
+    while (i < Vx.size())
+    {
+        cout << Vx[i] << endl;
+        i++;
+    }
 }
 void open_file(vector<int>& Vx, vector<int>& Vy, vector<int>& OM)
 {
@@ -85,9 +78,6 @@ void open_file(vector<int>& Vx, vector<int>& Vy, vector<int>& OM)
         i++;
     }
     //cout << i << endl;
-    
-   
-
     
 }
 
@@ -135,19 +125,70 @@ void correct_file(const ifstream& file)
 {
     if (!file)
     {
-       cout << "no input file" << endl;
+        //cout << "no input file" << endl;
         exit(-1);
     }
     else
     {
-        cout << "file is open" << endl;
+        //cout << "file is open" << endl;
     }
 }
-
-void push_data_to_output()
+/*
+void push_data_to_youBot(vector<int>& Vx, vector<int>& Vy, vector<int>& OM)
 {
-    string output = "output.txt";
+    unsigned int i = 0;
+    while (i < Vx.size())
+    {
+        longitudinalVelocity = Vx[i] * meter_per_second;
+        transversalVelocity = Vy[i] * meter_per_second;
+        angularVelocity = OM[i] * radian_per_second;
+        myYouBotBase->setBaseVelocity(longitudinalVelocity, transversalVelocity, angularVelocity);
+        SLEEP_MILLISEC(100);
+        i++;
+    }
+}
+*/
+/*
+void get_data_by_youBot()
+{
+    float trq1;
+    float trq2;
+    float trq3;
+    float trq4;
+    float omega_1;
+    float omega_2;
+    float omega_3;
+    float omega_4;
+
+    myYouBotBase->getBaseJoint(1).getData(sensedTorque); //снятие данных с датчиков колс
+    trq1 = -(sensedTorque.torque) / newton_meter;
+    myYouBotBase->getBaseJoint(2).getData(sensedTorque);
+    trq2 = (sensedTorque.torque) / newton_meter;
+    myYouBotBase->getBaseJoint(3).getData(sensedTorque);
+    trq3 = -(sensedTorque.torque) / newton_meter;
+    myYouBotBase->getBaseJoint(4).getData(sensedTorque);
+    trq4 = (sensedTorque.torque) / newton_meter;
+    
+    myYouBotBase->getBaseJoint(1).getData(sensedVelo);//Снятие угловых скоростей с датчиков колес
+    omega_1 = -sensedVelo.angularVelocity / radian_per_second;
+    myYouBotBase->getBaseJoint(2).getData(sensedVelo);
+    omega_2 = sensedVelo.angularVelocity / radian_per_second;
+    myYouBotBase->getBaseJoint(3).getData(sensedVelo);
+    omega_3 = -sensedVelo.angularVelocity / radian_per_second;
+    myYouBotBase->getBaseJoint(4).getData(sensedVelo);
+    omega_4 = sensedVelo.angularVelocity / radian_per_second;
+
+}
+*/
+void push_data_to_output(vector<int>& Vx, vector<int>& Vy, vector<int>& OM)
+{
+    string output = ("output.txt",std::ios::app);
     ofstream output_status_data;
     output_status_data.open(output, fstream::out);
+    int i = 0;
+    while (i< Vx.size())
+    {
+        
+    }
 }
 
